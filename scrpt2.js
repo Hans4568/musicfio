7ef4e801db2fbde0be17c8d84ca6132ee20ce096
Hans <herlyaguilar1717@gmail.com>
2021-10-04 22:12:29 -0500

    Primer commit

---
+++ scrpt2.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Gren day feat Oasis- Wonderwall",
+    artist: "music1",
+    img: "music-1",
+    src: "music1"
+  },
+  {
+    name: "You found me",
+    artist: "music",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "The reason",
+    artist: "music3",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Ghost of You",
+    artist: "music4",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Solo esas",
+    artist: "music5",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "music6",
+    artist: "music6",
+    img: "music-6",
+    src: "music-6"
+  },
+]
